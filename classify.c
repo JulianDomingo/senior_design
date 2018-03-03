de6096098984cf0645e29a70ef0690c9181b005e
JulianDomingo <julian.domingo@utexas.edu>
2018-03-03 01:38:45 -0600

    Updated classifier code

--- classify.c
+++ classify.c
@@ -3,38 +3,64 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <math.h>
+#include <string.h>
+#include <inttypes.h>
 
-// "inputs" is for testing purposes. We'll know the size of the input array
-// before classification of the curve occurs
-/*int root(int input, int n)*/
-/*{*/
-  /*return round(pow(input, 1./n));*/
-/*}*/
+#include "classify.h"
 
-uint8_t n = 6;
-uint8_t base = 2;
-/*size_t size = pow(base, n); */
-/*uint16_t inputs[size]; */
+double root(double input, double n)
+{
+  return pow(input, (1.0 / n));
+}
 
-uint32_t test = 1;
-printf("%d", test);
+int main()
+{
+    uint8_t n = 12;
+    uint8_t base = 2;
 
-// Try linear
-// [Samuel's code here]
+    // The size of our input array will be be pre-determined in our final code.
+    size_t size = pow(base, n); 
+    uint64_t inputs[size]; 
 
-// Try n = 2 to n = 4 degree polynomials
-/*for (int n = 2; n <= 4; n += 1) {*/
-    // Potentially change to transform only a subset of the points instead of the entire curve
-    /*uint16_t transformed[size];*/
-    /*memcpy(transformed, inputs, size * sizeof(uint16_t));*/
+    for (int value = 0; value < size; value += 1) 
+    {
+        inputs[value] = value; 
+        printf("%d\n", inputs[value]);
+    }
 
-    // Compute the appropriate transformation.
-    /*for (int i = 0; i < size; i += 1) {*/
-        /*transformed[i] = */
-    /*}*/
+    // Try linear
 
-    // For each transformed input array, try to fit to a perfectly linear curve and a monotonically increasing curve (bsearch)
-/*}*/
+    // Try n = 2 to n = 4 degree polynomials
+    for (int n = 2; n <= 4; n += 1) {
+        uint64_t transformed[size];
+        memcpy(transformed, inputs, size * sizeof(uint64_t));
 
-// Try exponential
-// TODO: Figure out how to do natural log of a value (no natural log functionality for the board we're using)
+        // Compute the appropriate transformation.
+        for (int i = 0; i < size; i += 1) {
+            // TODO: Potentially change to transform only a subset of the points instead of the entire curve.
+            // Take the nth root of the original input
+            transformed[i] = root(inputs[i], n); 
+            printf("%f\n", transformed[i]);
+        }
+
+        // For each transformed input array, try to fit to a perfectly linear curve
+        // TODO: Get corresponding input value from target voltage using constant mapping of a 
+        // perfectly linear curve, and use the input value to obtain a corresponding output
+        // voltage using the transformed curve. Compare the two output voltages; if their
+        // difference is within the error margin we specify:
+        // 1. Use the original (untransformed) input value as our trim solution (the input value is a bit combination).
+        // 2. Classify the original input curve as an "n" degree polynomial.
+
+        // NOTE: unnecessary to "re-transform" the curve since our original input signal is
+        // untampered.
+
+        // Otherwise, we try fitting the input signal as a monotonically inc./dec. curve using a binary search to find the best bit combination to trim.
+        // TODO: Perform binary search on the "transformed" array (terminates once error margin reached)
+        // If a combination resulting in an acceptable error margin is found, perform steps 1. and 2. as explained for the strictly linear case.
+    }
+
+    // Try exponential
+    // TODO: Figure out how to do natural log of a value (no natural log functionality for the board we're using)
+
+    return 0;
+}
--- classify.h
+++ classify.h
@@ -1,2 +1,2 @@
 // Function prototypes
-uint8_t pow(uint8_t base, uint8_t n);
+double root(double input, double n);
